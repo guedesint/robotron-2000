c492ea05bbc64a9dbadf174d5201a77591fea28e
GuedesPC <guedesint@gmail.com>
2023-01-04 18:29:16 -0300

    rever o foreach

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-const robotron = document.querySelector("#robotron");
-
 
 const controle = document.querySelectorAll(".controle-ajuste");
 
 controle.forEach((elemento) => {
-  
-})
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.textContent, evento.target.parentNode)
+  })
+});
 
 
 
 
 
-    function manipulaDados(operacao) {
-            if (operacao === "subtrair") {
-                inputContador.value = parseInt(inputContador.value) - 1;
+    function manipulaDados(operacao, controle) {
+
+            var peca = controle.querySelector(".controle-contador")
+
+            if (operacao === "-") {
+                peca.value = parseInt(peca.value) - 1;
             }else {
-                inputContador.value = parseInt(inputContador.value) + 1;
+                peca.value = parseInt(peca.value) + 1;
             }
         }
